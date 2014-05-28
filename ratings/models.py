c41ace75b770e82792c536e6719973c43f88e22c
Parker Tew <ptew@mit.edu>
2014-05-28 03:04:39 +0000

    database update to handle survey

--- ratings/models.py
+++ ratings/models.py
@@ -24,6 +24,18 @@ class User(models.Model):
 	user_id = models.IntegerField(default=0)
 	ip_address = models.CharField(default="0.0.0.0", max_length=20)
 	participation_timestamp = models.DateTimeField('date published')
+	age = models.IntegerField(default=0)
+	gender = models.IntegerField(default=0)
+	experience = models.IntegerField(default=0)
+	investment_familiarity = models.IntegerField(default=0)
+	firm = models.IntegerField(default=0)
+	past_performance = models.IntegerField(default=0)
+	zillow = models.IntegerField(default=0)
+	costar = models.IntegerField(default=0)
+	netease = models.IntegerField(default=0)
+	athena = models.IntegerField(default=0)
+	reputation_influence = models.IntegerField(default=0)
+	affiliation_influence = models.IntegerField(default=0)
 
 class Vote(models.Model):
 	profile = models.ForeignKey(Profile)
@@ -47,3 +59,12 @@ class Next_Timestamp(models.Model):
 	user_id = models.IntegerField(default=0)
 	timestamp = models.DateTimeField('next timestamp')
 	sequence_number = models.IntegerField(default=0)
+
+class Advice_Feedback(models.Model):
+	user_id = models.IntegerField(default=0)
+	timestamp = models.DateTimeField('next timestamp')
+	sequence_number = models.IntegerField(default=0)
+	value = models.IntegerField(default=0)
+	advice = models.ForeignKey(Advice)
+	profile = models.ForeignKey(Profile)
+
