9c859622d10073c374ceb53ab8fb77cfa5cdc54d
Parker Tew <ptew@mit.edu>
2014-05-29 03:29:41 +0000

    survey finished

--- ratings/ajax.py
+++ ratings/ajax.py
@@ -39,3 +39,49 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+@dajaxice_register
+def save_advice_feedback(request, advice_id, profile_id, id_number, value):
+	prof = get_object_or_404(Profile, profile_number = profile_id)
+	advice = get_object_or_404(Advice, pk = advice_id)
+	time = timezone.now()
+
+	feedback = Advice_Feedback(user_id=id_number, profile=prof, timestamp=time, value=value, advice=advice)
+	feedback.save()
+	return simplejson.dumps({'message':'advice feedback: %s' % value})
+
+@dajaxice_register
+def survey_batch_one(request, id_number, age, gender, experience):
+	user = get_object_or_404(User, user_id=id_number)
+	user.age = age
+	user.gender = int(gender)
+	user.experience = experience
+	user.save()
+	return simplejson.dumps({'message':'user batch one update successful'})
+
+@dajaxice_register
+def survey_batch_two(request, id_number, investment_familiarity, firm, past_performance):
+	user = get_object_or_404(User, user_id=id_number)
+	user.investment_familiarity = investment_familiarity
+	user.firm = firm
+	user.past_performance = past_performance
+	user.save()
+	return simplejson.dumps({'message':'user batch two update successful'})
+
+@dajaxice_register
+def survey_batch_three(request, id_number, zillow, costar, netease, athena):
+	user = get_object_or_404(User, user_id=id_number)
+	user.zillow = int(zillow)
+	user.costar = int(costar)
+	user.netease = int(netease)
+	user.athena = int(athena)
+	user.save()
+	return simplejson.dumps({'message':'user batch three update successful'})
+
+@dajaxice_register
+def survey_batch_four(request, id_number, reputation_influence, affiliation_influence):
+	user = User.objects.filter(user_id=id_number)
+	user.reputation_influence = int(reputation_influence)
+	user.affiliation_influence = int(affiliation_influence)
+	user.save()
+	return simplejson.dumps({'message':'user batch four update successful'})
